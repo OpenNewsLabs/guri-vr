b947e0025bb98f8267104ea9727b961568613137
Dan Zajdband <dan.zajdband@gmail.com>
2016-07-25 11:40:29 -0400

    Added service worker and build command

---
+++ client/webpack.config.dev.js
@@ -0,0 +1,24 @@
+
+var path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, '..', 'public'),
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      { loader: 'babel-loader', test: /\.js$/, exclude: /node_modules/ },
+      { loader: 'json-loader', test: /\.json$/, exclude: /node_modules/ }
+    ]
+  },
+  resolve: {
+    root: [ path.resolve('./src') ],
+    alias: {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
+  plugins: []
+};
