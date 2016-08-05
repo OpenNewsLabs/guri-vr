b2e06d341b9150a174ff1e794573912894b2f380
Dan Zajdband <dan.zajdband@gmail.com>
2016-08-05 12:06:04 -0400

    Added logo <3

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,7 +11,7 @@ import Previewer from 'components/Previewer';
 const defaultEditorText = `
 You not need to be a coder to create VR experiences. The GuriVR editor interprets your words and creates the world you described.
 
-Try adding some scenes. For example my first scene will last 500 seconds and display the following text: "Guri is cooooool!" and a panorama located at https://s3.amazonaws.com/gurivr/pano.jpg
+Try adding some scenes. For example my first scene will last 500 seconds and display an image located at https://gurivr.com/logo.svg along with a text saying: "Guri is cool!" to my left and a panorama located at https://s3.amazonaws.com/gurivr/pano.jpg
 
 Try adding more scenes!
 
