f3c4b44941dd3dc3e51373ea9b1b2271b2906e64
Dan Zajdband <dan.zajdband@gmail.com>
2016-11-16 15:11:47 -0500

    Implemented language switch

--- client/src/components/LangSwitch.js
+++ client/src/components/LangSwitch.js
@@ -1,7 +1,7 @@
 
 import { h } from 'preact'
 import { getLocale, setLocale } from 'services/i18n'
-import { style } from 'glamor'
+import { style, merge, select } from 'glamor'
 
 const locales = {
   'en': { value: 'en-US', name: 'English' },
@@ -9,9 +9,16 @@ const locales = {
 }
 
 export default () => (
-  <div onClick={() => setLocale(locales[getLocale()].value)} {...styles.container}>
-    <span {...styles.name}>{locales[getLocale()].name}</span>
-    <span {...styles.arrowDown} />
+  <div {...styles.container}>
+    <div onClick={() => setLocale(locales[getLocale()].value)} {...styles.button} {...select(':hover + div', { display: 'block' })}>
+      <span {...styles.name}>{locales[getLocale()].name}</span>
+      <span {...styles.arrowDown} />
+    </div>
+    <div {...styles.content}>
+      {Object.keys(locales).filter(l => getLocale() !== l).map(l => (
+        <span onClick={() => setLocale(locales[l].value)}>{locales[l].name}</span>
+      ))}
+    </div>
   </div>
 )
 
@@ -20,16 +27,19 @@ const styles = {
     marginTop: 20,
     float: 'right',
     marginRight: 20,
-    color: '#fff',
     fontSize: 14,
-    textTransform: 'uppercase',
+    textTransform: 'uppercase'
+  }),
+
+  button: style({
+    color: '#fff',
     fontWeight: 'bold',
     backgroundColor: 'rgba(0, 0, 0, .2)',
     borderRadius: 3,
     padding: '8px 10px',
+    cursor: 'pointer',
     display: 'flex',
-    alignItems: 'center',
-    cursor: 'pointer'
+    alignItems: 'center'
   }),
 
   name: style({
@@ -42,5 +52,17 @@ const styles = {
     borderLeft: '5px solid transparent',
     borderRight: '5px solid transparent',
     borderTop: '5px solid #fff'
+  }),
+
+  content: style({
+    backgroundColor: 'rgba(0, 0, 0, .6)',
+    padding: 5,
+    color: '#fff',
+    borderRadius: 0,
+    cursor: 'pointer',
+    display: 'none',
+    ':hover': {
+      display: 'block'
+    }
   })
 }
