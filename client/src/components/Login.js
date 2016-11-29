60f54be60388074ba5a372c6efda679b4b3f2f67
Dan Zajdband <dan.zajdband@gmail.com>
2016-11-29 17:23:52 -0500

    Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,11 @@
 
 import { h, Component } from 'preact'
-import { TextField, Card, Button } from 'preact-mdl'
+import { Link } from 'preact-router'
+import Logo from 'components/Logo'
 import { sendVerification } from 'services/datalayer'
-import Radium from 'radium'
+import { style } from 'glamor'
 import t from 'services/i18n'
 
-@Radium
 export default class Login extends Component {
   constructor (props) {
     super(props)
@@ -14,6 +14,8 @@ export default class Login extends Component {
       email: '',
       sent: false
     }
+
+    this.onSubmit = this.onSubmit.bind(this)
   }
 
   onSubmit (evt) {
@@ -26,51 +28,90 @@ export default class Login extends Component {
 
   renderSent () {
     return (
-      <div style={styles.sent}>
-        <Card.Text>{t('login.sent')}</Card.Text>
-      </div>
+      <p {...styles.text}>{t('login.sent')}</p>
     )
   }
 
   renderForm () {
     const { email } = this.state
     return (
-      <form onSubmit={this.onSubmit.bind(this)}>
-        <Card.Text>
-          <TextField placeholder='me@gmail.com'
-            label='E-mail'
-            type='email'
-            floating-label
-            required
-            value={email} onInput={this.linkState('email')} autofocus />
-        </Card.Text>
-        <Card.Actions>
-          <Button raised colored type='submit'>{t('login.login')}</Button>
-        </Card.Actions>
+      <form onSubmit={this.onSubmit}>
+        <p {...styles.text}>{t('login.text')}</p>
+        <div {...styles.emailGroup}>
+          <label for='email' {...styles.label}>E-mail</label>
+          <input type='email' id='email' required placeholder='me@gmail.com'
+            value={email} {...styles.email} onInput={this.linkState('email')}
+            autofocus />
+        </div>
+        <button {...styles.button} type='submit'>{t('login.login')}</button>
       </form>
     )
   }
 
   render (props, { sent }) {
     return (
-      <div style={styles.container}>
-        <Card shadow={2}>
-          {sent ? this.renderSent() : this.renderForm()}
-        </Card>
+      <div {...styles.container}>
+        <Link href='/' {...styles.logoContainer}>
+          <Logo color='#5A33A2' />
+        </Link>
+        {sent ? this.renderSent() : this.renderForm()}
       </div>
     )
   }
 }
 
 const styles = {
-  container: {
-    margin: '50px auto'
-  },
-  sent: {
-    flex: 1,
+  container: style({
+    margin: '50px auto',
+    width: 350
+  }),
+
+  logoContainer: style({
+    justifyContent: 'center',
+    display: 'flex'
+  }),
+
+  label: style({
+    color: '#5A33A2',
+    fontSize: 12,
+    marginBottom: 5
+  }),
+
+  email: style({
+    fontSize: 16,
+    border: 0,
+    borderBottom: '3px solid #5A33A2'
+  }),
+
+  emailGroup: style({
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    marginTop: 40
+  }),
+
+  text: style({
+    fontSize: 16,
+    color: '#999',
+    marginTop: 50,
+    textAlign: 'center'
+  }),
+
+  button: style({
+    marginTop: 50,
+    display: 'block',
+    borderRadius: 5,
+    backgroundColor: '#5A33A2',
+    border: 'none',
+    color: '#fff',
+    width: '100%',
+    fontSize: 18,
+    textTransform: 'uppercase',
+    padding: 14,
+    fontWeight: 500,
+    boxShadow: '0 2px 3px rgba(90,53,162,0.3)',
+    cursor: 'pointer',
+    ':hover': {
+      backgroundColor: '#734EBB'
+    }
+  })
 }
