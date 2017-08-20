6ed7b13739ca743e30710cdeb0f091f49a65509d
Jerome Etienne <jerome.etienne@gmail.com>
2017-08-20 10:17:54 +0100

    more work

--- cmdline/cmdline.js
+++ cmdline/cmdline.js
@@ -1,21 +1,21 @@
 var buildStory = require('../client/shared/story-builder')
 var nlp = require('../client/shared/nlp')
-const commandLineArgs = require('command-line-args')
+var commandLineArgs = require('command-line-args')
 
 //////////////////////////////////////////////////////////////////////////////
 //                Code Separator
 //////////////////////////////////////////////////////////////////////////////
 
-var options = {
-        title : 'my super title',
-        text : 'A sky at sunset',
-        mode : 'vr',
-}
-
-const optionDefinitions = [
-        { name: 'title', alias: 't', type: Boolean },
+// Attempts at cmdline option parsing...
+// - needs a -h --help
+// - needs error message when error
+// - i picked this package at random. aka it is the first i saw
+// - may need a more knowledgable choise
+var optionDefinitions = [
+        { name: 'title', alias: 't', type: String },
         { name: 'mode', alias: 'm', type: String },
-        { name: 'text', type: String, defaultOption: true },
+        { name: 'output', alias: 'o', type: String },
+        { name: 'text', type: String },
 ]
 
 var options = commandLineArgs(optionDefinitions)
@@ -29,9 +29,12 @@ var story = {
         chapters: chapters,
         mode: options.mode
 }
+console.log(JSON.stringify(story, null, '\t'))
 var html = buildStory(story)
 
 //////////////////////////////////////////////////////////////////////////////
 //                output html page
 //////////////////////////////////////////////////////////////////////////////
-console.log(html)
+
+// dump the html into a file
+require('fs').writeFileSync(options.output, html, 'utf8');
